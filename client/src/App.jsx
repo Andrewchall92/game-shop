81050ea28566674e253f8ece2663629e0652b05b
Andrew Hall <andrewchall92@gmail.com>
2023-12-09 01:13:39 -0800

    Moved tailwind install into the client folder and properly configured it

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,13 @@
 import './App.css'
-import React from 'react'
 
-function App() {
+export default function App() {
   return (
     <>
-    <h1 className="text-3xl font-bold underline">Hello World</h1>
+     <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
     </>
   )
 }
 
-export default App
+
