259ab7ba9e4f4c62cef5c35d170a0b1af377dd27
Andy <84168944+AZurek17@users.noreply.github.com>
2023-12-09 15:01:15 -0700

    added import links for contact and review in main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -11,7 +11,8 @@ import Login from './pages/Login';
 import Signup from './pages/Signup';
 import Success from './pages/Success';
 import OrderHistory from './pages/OrderHistory';
-
+import Contact from "./pages/Contact";
+import Review from "./pages/Review";
 
 const router = createBrowserRouter([
   {
