52b932c755bacb7b90464e53cc72459e239bdf87
Anthony Nguyen <anthonynguye56@gmail.com>
2023-12-08 20:46:46 -0800

    index models

---
+++ server/models/index.js
@@ -0,0 +1,8 @@
+import { Coupon }  from './Coupon';
+import { Category }  from './Category';
+import { Order }  from './Order';
+import { Product }  from './Product';
+import { Review }  from './Review';
+import { User }  from './User';
+
+module.exports = { Coupon, Category, Order, Product, Review, User };
