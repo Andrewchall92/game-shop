d05b4d339701e98cff56f5c1548c946244f7795a
Anthony Nguyen <anthonynguye56@gmail.com>
2023-12-08 21:10:29 -0800

    implemented review model

---
+++ server/models/Review.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
+
+const { Schema, model } = mongoose;
+
+const reviewSchema = new Schema({
+    commentText: {
+        type: String,
+        required: true,
+        minlength: 1,
+        maxlength: 280,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: (timestamp) => dateFormat(timestamp),
+    },
+    user: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'User',
+        }
+    ],
+});
+
+
+const Review = mongoose.model('Review', reviewSchema);
+
+module.exports = Review;
