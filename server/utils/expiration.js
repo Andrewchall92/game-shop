1ccf5103144316f6c50e3253d0ac69040f255ec6
Anthony Nguyen <anthonynguye56@gmail.com>
2023-12-08 22:08:05 -0800

    debugged helper expiration function

--- server/utils/expiration.js
+++ server/utils/expiration.js
@@ -1,15 +1,23 @@
-const mongoose = require('mongoose');
 
 module.exports = (timestamp) => {
 
-    let currentDate = new Date();
+    let currentDate = new Date()+'';
+ 
+    const addSeven = parseInt(currentDate.split(' ')[2])+7;
 
+    if(addSeven > 30){
+        addSeven = addSeven - 30;
+    }
 
-    console.log(currentDate);
-
+    console.log(addSeven)
     // Set the expiration date (in this example, adding 7 days to the current date)
-    let expirationDate = new Date(currentDate);
-    expirationDate.setDate(currentDate.getDate() + 7);
+    let stringDate = new Date(currentDate).toString();
+    stringDate.setDate(addSeven);
 
+    let expirationDate = stringDate.toString().slice(0, 15).trim();
+
+    console.log(expirationDate);
     return expirationDate;
 };
+
+
