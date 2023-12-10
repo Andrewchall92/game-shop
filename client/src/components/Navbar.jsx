fcbbe19e9c5e38d076b7c64b8e0b5dbb3acd9389
Master <nghiemxthai@gmail.com>
2023-12-10 05:49:43 -0800

    added icon and badge to navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,10 +1,60 @@
-import { AppBar } from '@mui/material';
-import React from 'react';
+import styled from "@emotion/styled";
+import {
+  AppBar,
+  Badge,
+  Box,
+  InputBase,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import React from "react";
+import VideogameAssetIcon from "@mui/icons-material/VideogameAsset";
+import NotificationsIcon from '@mui/icons-material/Notifications';
+import AccountBoxIcon from '@mui/icons-material/AccountBox';
+
+const StyledToolbar = styled(Toolbar)({
+  display: "flex",
+  justifyContent: "space-between",
+});
+
+const Search = styled("div")(({ theme }) => ({
+  backgroundColor: "white",
+  padding: " 0 10px",
+  borderRadius: theme.shape.borderRadius,
+  width: "40%",
+}));
+
+const Icons = styled(Box)(({ theme }) => ({
+display: "flex",
+gap:"20px"
+}));
 
 export const Navbar = () => {
-return (
-    <AppBar> Navbar </AppBar>
-    )
+  return (
+    <AppBar position="sticky">
+      <StyledToolbar>
+        <Typography variant="h6" sx={{ display: { xs: "none", sm: "block" } }}>
+          
+          GAME OVER
+        </Typography>
+        <VideogameAssetIcon sx={{ display: { sm: "none", xs: "block" } }} />
+        <Search>
+          
+          <InputBase placeholder="search..." />
+        </Search>
+        <Icons>
+         
+          <Badge badgeContent={4} color="error">
+            <NotificationsIcon color="action" />
+          </Badge>
 
-}
-    
+          <Badge badgeContent={4} color="error">
+            <AccountBoxIcon color="action" />
+          </Badge>
+
+
+        </Icons>
+      </StyledToolbar>
+    </AppBar>
+  );
+};
