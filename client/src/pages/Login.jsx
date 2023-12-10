39fea12d0af38e41d3eeaa06d54cfbefccd75de8
Andy <84168944+AZurek17@users.noreply.github.com>
2023-12-09 21:20:58 -0700

    updated login images to pull random videogames photos

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -49,7 +49,7 @@ function Login () {
               sm={4}
               md={7}
               sx={{
-                backgroundImage: 'url(https://source.unsplash.com/random?wallpapers)',
+                backgroundImage: 'url(https://source.unsplash.com/random?videogames)',
                 backgroundRepeat: 'no-repeat',
                 backgroundColor: (t) =>
                   t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
