8c729c7268f9d84ad74cca4fbbc7eacf679dcb57
Andy <84168944+AZurek17@users.noreply.github.com>
2023-12-09 21:58:00 -0700

    added functinos to helpers.js in client utils

--- client/src/pages/NoMatch.jsx
+++ client/src/pages/NoMatch.jsx
@@ -1,8 +1,12 @@
+import "../styles/NoMatch/styles.css";
+
 function NoMatch(){
 
     return(
-        <div>
+        <div id="errorBg">
+        <div >
         <h1>404 Page Not Found</h1>
+        </div>
         </div>
     )
 };
