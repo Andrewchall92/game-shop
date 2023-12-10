ec89e47b974e57091b79c7e9a2dd392a942ca594
Master <nghiemxthai@gmail.com>
2023-12-10 05:58:53 -0800

    added profile to navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -4,13 +4,15 @@ import {
   Badge,
   Box,
   InputBase,
+  Menu,
+  MenuItem,
   Toolbar,
   Typography,
 } from "@mui/material";
-import React from "react";
+import React, { useState } from "react";
 import VideogameAssetIcon from "@mui/icons-material/VideogameAsset";
-import NotificationsIcon from '@mui/icons-material/Notifications';
-import AccountBoxIcon from '@mui/icons-material/AccountBox';
+import NotificationsIcon from "@mui/icons-material/Notifications";
+import AccountBoxIcon from "@mui/icons-material/AccountBox";
 
 const StyledToolbar = styled(Toolbar)({
   display: "flex",
@@ -25,34 +27,46 @@ const Search = styled("div")(({ theme }) => ({
 }));
 
 const Icons = styled(Box)(({ theme }) => ({
-display: "flex",
-gap:"20px"
+  display: "flex",
+  gap: "20px",
 }));
 
 export const Navbar = () => {
+  const [open, setOpen] = useState(false);
+
   return (
     <AppBar position="sticky">
       <StyledToolbar>
         <Typography variant="h6" sx={{ display: { xs: "none", sm: "block" } }}>
-          
           GAME OVER
         </Typography>
         <VideogameAssetIcon sx={{ display: { sm: "none", xs: "block" } }} />
         <Search>
-          
           <InputBase placeholder="search..." />
         </Search>
         <Icons>
-         
           <Badge badgeContent={4} color="error">
             <NotificationsIcon color="action" />
           </Badge>
-
-          <Badge badgeContent={4} color="error">
-            <AccountBoxIcon color="action" />
-          </Badge>
-
-
+          <Menu
+            id="demo-positioned-menu"
+            aria-labelledby="demo-positioned-button"
+            open={open}
+            onClose={(e)=>  setOpen(false)}
+            anchorOrigin={{
+              vertical: "top",
+              horizontal: "right",
+            }}
+            transformOrigin={{
+              vertical: "top",
+              horizontal: "right",
+            }}
+          >
+            <MenuItem>Profile</MenuItem>
+            <MenuItem>My account</MenuItem>
+            <MenuItem>Logout</MenuItem>
+          </Menu>
+          <AccountBoxIcon onClick={(e)=>{setOpen(true)}} color="action" />
         </Icons>
       </StyledToolbar>
     </AppBar>
