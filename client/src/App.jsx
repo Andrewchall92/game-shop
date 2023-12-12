fe71a9ca8f3c366806b7768b3c507c3f1ae580ea
Andy <84168944+AZurek17@users.noreply.github.com>
2023-12-12 08:44:18 -0700

    added authlink to app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,32 @@
 import { Outlet } from "react-router-dom";
 import { theme } from "./assets/theme";
 import { ThemeProvider } from "@mui/material";
-
+import './index.css'
 import {
   ApolloClient,
   InMemoryCache,
   ApolloProvider,
   createHttpLink,
 } from '@apollo/client';
-import './index.css'
+import { setContext } from '@apollo/client/link/context';
 
+
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
 
 const client = new ApolloClient({
-  link:  '/graphql',
+  link:  authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
 
