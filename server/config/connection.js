f9d6f4a632f04493740c2027716bd3aef5e35e48
Andrew Hall <andrewchall92@gmail.com>
2023-12-14 15:48:35 -0800

    seeds

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 require("dotenv").config();
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/game-shop');
+console.log(process.env.MONGODB_URI);
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/game-shop').catch(err => console.log(err))
 
 module.exports = mongoose.connection;
