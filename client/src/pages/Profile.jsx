a42e04581371d4339ace3a0cc8814b54e33a3b77
Master <nghiemxthai@gmail.com>
2023-12-13 17:02:17 -0800

    added profile page

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -2,44 +2,41 @@ import React from "react";
 import { Box, Stack, Typography, Container } from "@mui/material";
 import { Navbar } from "../components/Navbar";
 import { Sidebar } from "../components/Sidebar";
-import {TOGGLE_CART} from "../utils/actions";
+import { TOGGLE_CART } from "../utils/actions";
 import { useStoreContext } from "../utils/GlobalState";
 import Auth from "../utils/auth";
 
 const Profile = () => {
-    const [ state, dispatch] = useStoreContext();
-    const toggleCart = () => {
-        dispatch({ type: TOGGLE_CART });
-    };
-    
+  const [state, dispatch] = useStoreContext();
+  const toggleCart = () => {
+    dispatch({ type: TOGGLE_CART });
+  };
 
-    return (
-        
-        <Box>
-            <Navbar toggleCart={toggleCart} />
-            <Stack direction="row" spacing={2} justifyContent="space-between">
-               <Container>
-{Auth.loggedIn() ? (
-                <Box flex={4} p={2} className="main-display">
-                    <Typography variant="h4">Log in</Typography>
-                </Box>
+  return (
+    <Box>
+      <Navbar toggleCart={toggleCart} />
+      <Stack direction="row" spacing={2} justifyContent="space-between">
+        <Container>
 
-        ) : (
+          {Auth.loggedIn() ? (
             <Box flex={4} p={2} className="main-display">
-                    <Typography variant="h4">Not Log in</Typography>
-                </Box>
-        )}
-               </Container>
-                
-            </Stack>
-        </Box>
-    );
+              <Typography variant="h4">Log in</Typography>
+            </Box>
+
+
+          ) : (
+
+
+
+            <Box flex={4} p={2} className="main-display">
+              <Typography variant="h4">Please Log In To View Profile</Typography>
+              
+            </Box>
+          )}
+        </Container>
+      </Stack>
+    </Box>
+  );
 };
 
-
 export default Profile;
-
-
-
-
-
