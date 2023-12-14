92c69285d5989e80b2b5a45e8d2be4b50ae9abe6
Master <nghiemxthai@gmail.com>
2023-12-14 00:25:02 -0800

    Single product page in progress

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -1,21 +1,27 @@
-import React, { useState } from "react";
+import React from "react";
 import Button from "@mui/material/Button";
 import Grid from "@mui/material/Grid";
 import Divider from "@mui/material/Divider";
 import Typography from "@mui/material/Typography";
-import { CardMedia, Checkbox, Box, Stack, Container } from "@mui/material";
+import { CardMedia, Checkbox , Box, Stack, Container} from "@mui/material";
 import { FavoriteBorder, Favorite, ExpandMore } from '@mui/icons-material';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import Collapse from '@mui/material/Collapse';
-import { CardHeader } from '@mui/material';
+import {useState} from 'react';
+import {CardHeader} from '@mui/material';
 
-export function ProductCard({ name, _id, price, description, image, addToCart }) {
-  const [expanded, setExpanded] = useState(false);
+// function generateRandomNumber() {
+//   const randomNumber = Math.floor(Math.random() * 900000000) + 100000000;
+//   return randomNumber.toString();ç
+
+export function ProductCard({ name, _id,  price, description, image, addToCart }) {
+
+const [expanded, setExpanded] = useState(false);
 
   const handleExpandClick = () => {
     setExpanded(!expanded);
   }
-
+  // console.log(name, _id, price, description, image);
   return (
     <Box
       key={_id}
@@ -46,17 +52,19 @@ export function ProductCard({ name, _id, price, description, image, addToCart })
           </Grid>
         </Grid>
 
+
         <Collapse in={expanded} timeout="auto" unmountOnExit>
-          <Typography color="text.secondary" variant="body2" sx={{ display: { sm: "none", lg: "block" }, fontSize: '0.7rem' }}>
-            {description}
-          </Typography>
-        </Collapse>
+        <Typography color="text.secondary" variant="body2" sx={{display:{sm:"none", lg:"block"}, fontSize: '0.7rem'}}>
+          {description}
+        
+        </Typography>
+        </Collapse> 
       </Box>
       <Divider variant="middle" />
-
-      <Stack direction="row" spacing={2} justifyContent="space-between">
+      
+      <Stack direction="row" spacing={2} justifyContent="space-between" >
         <Checkbox icon={<FavoriteBorder />} checkedIcon={<Favorite />} />
-        <ExpandMore
+          <ExpandMore
           expand={expanded}
           onClick={handleExpandClick}
           aria-expanded={expanded}
@@ -64,14 +72,16 @@ export function ProductCard({ name, _id, price, description, image, addToCart })
         >
           <ExpandMoreIcon />
         </ExpandMore>
+        
       </Stack>
-
+     
       <Box sx={{ m: 2 }}></Box>
       <Box sx={{ mt: 3, ml: 1, mb: 1 }}>
-        <Stack justifyContent='space-evenly'>
-          <Button onClick={() => addToCart({ name, _id, price, description, quantity: 1, image })}> Add to cart </Button>
-          <Button onClick={() => { window.location = `/products/${_id}` }}> View this product </Button>
-        </Stack>
+      <Stack justifyContent='space-evenly'>
+      <Button onClick={() => addToCart({ name, _id, price, description, quantity: 1, image })}> Add to cart </Button>
+         <Button onClick={()=>{window.location=`/products/${_id}`}} > View this product </Button>
+      </Stack>
+     
       </Box>
     </Box>
   );
--- client/src/pages/SingleProduct.jsx
+++ client/src/pages/SingleProduct.jsx
@@ -1,37 +1,120 @@
-import { Box, Typography, Stack, Container } from '@mui/material';
-import React from 'react'
-import {useParams} from 'react-router-dom';
-import { Navbar } from '../components/Navbar';
+import {
+    Box,
+    Typography,
+    Stack,
+    Container,
+    Card,
+    CardMedia,
+    CardContent,
+    Button,
+    Grid,
+} from "@mui/material";
+import React from "react";
+import { useParams } from "react-router-dom";
+import { Navbar } from "../components/Navbar";
+import CardActions from "@mui/material/CardActions";
+import { useQuery } from '@apollo/client';
+import { QUERY_SINGLE_PRODUCT } from "../utils/queries";
 
-const OneProductPage = () => { 
+const OneProductPage = () => {
     const { id } = useParams();
+
+    const { loading, data } = useQuery(QUERY_SINGLE_PRODUCT, {
+        variables: { id: id }
+    });
+
+    const product = data?.product || {};
+    console.log(product);
+
     return (
         <Box>
-        <Navbar  />
-        <Stack direction="row" spacing={2} justifyContent="space-between" sx={{paddingTop:'3%'}}>
-          <Container>
-            <Box flex={4} p={2} className="main-display" sx={{backgroundColor:"pink"}}>
-              <Typography variant="h4">Product Info</Typography>
+            <Navbar />
+            { product ? (
+            <Stack
+                direction="row"
+                spacing={2}
+                justifyContent="space-between"
+                sx={{ paddingTop: "3%" }}
+            >
+                
+                <Container>
+                    
+                        <Box
+                            flex={4}
+                            p={2}
+                            className="main-display"
 
+                            display='flex'
+                            justifyContent='center'
+                        >
+                            <Grid container spacing={2}>
+                                <Grid item xs={12} sm={6}>
+                                    <Typography variant="h4" id='productInfo'>Game Info</Typography>
+                                </Grid>
+                            </Grid>
 
+                            <Card sx={{ maxWidth: 345 }}>
+                                <CardMedia
+                                    sx={{ height: 400 }}
+                                    image={product.image}
+                                    title="green iguana"
+                                />
+                                <CardContent>
+                                    <Typography gutterBottom variant="h5" component="div">
+                                        {product.name}
+                                    </Typography>
+                                    <Typography variant="body2" color="text.secondary">
+                                        {product.description}
+                                    </Typography>
+                                </CardContent>
+                                <CardActions>
+                                    <Button size="small">Share</Button>
+                                    <Button size="small">Learn More</Button>
+                                </CardActions>
+                            </Card>
+                        </Box>
+                    
+                </Container>
 
-            </Box>
-          </Container>
+                <Container>
+                    <Box
+                        flex={4}
+                        p={2}
+                        className="main-display"
+                        sx={{ backgroundColor: "skyblue" }}
+                    >
+                         <Grid container spacing={2}>
+                                <Grid item xs={12} sm={6}>
+                                    <Typography variant="h4" id='productInfo'>Write A review</Typography>
+                                </Grid>
+                            </Grid>
+                    </Box>
+                </Container>
 
-          <Container>
-            <Box flex={4} p={2} className="main-display" sx={{backgroundColor:"skyblue"}}>
-              <Typography variant="h4"> Add Comment</Typography>
-            </Box>
-          </Container>
-
-          <Container>
-            <Box flex={4} p={2} className="main-display" sx={{backgroundColor:"yellow"}}>
-              <Typography variant="h4"> Create Comment </Typography>
-            </Box>
-          </Container>
-        </Stack>
-      </Box>
-    )
+                <Container>
+                    <Box
+                        flex={4}
+                        p={2}
+                        className="main-display"
+                        sx={{ backgroundColor: "yellow" }}
+                    >
+                        <Grid container spacing={2}>
+                                <Grid item xs={12} sm={6}>
+                                    <Typography variant="h4" id='productInfo'> Others Review </Typography>
+                                </Grid>
+                            </Grid>
+                    </Box>
+                </Container>
+                
+            </Stack>
+            ) : (
+                        <Box>
+                            <Typography variant="h4">No Product Found</Typography>
+                        </Box>
+                    )}
+        </Box>
+    );
 };
 
 export default OneProductPage;
+
